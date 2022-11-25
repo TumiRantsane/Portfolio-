9a8d70b6fca0f6f5197ea9ce1b7f3343576bc94f
danielsafari143 <113584219+danielsafari143@users.noreply.github.com>
2022-11-25 14:17:02 +0100

    Update functionality.js
    Update functionality js

--- js/functionality.js
+++ js/functionality.js
@@ -9,7 +9,23 @@ menuLinks.forEach((menuLink) => {
   menuLink.addEventListener('click', closeMenu);
 });
 
-
+document.addEventListener('DOMContentLoaded', () => {
+  createModal();
+  const buttonsProject = document.querySelectorAll('.works__card button');
+  const closeIcon = document.querySelector('.closeModal');
+  const modal = document.querySelector('.modal');
+  const backgroundModal = document.querySelector('.overlay');
+  const projectName = document.querySelector('.project-name');
+  const projectFeature = document.querySelector('.project-feature');
+  const projectBack = document.querySelector('.project-background');
+  const projectYear = document.querySelector('.project-year');
+  const projectImage = document.querySelector('.project-image');
+  const projectDescription = document.querySelector('.project-description');
+  const projectTool1 = document.querySelector('.project-tool1');
+  const projectTool2 = document.querySelector('.project-tool2');
+  const projectTool3 = document.querySelector('.project-tool3');
+  const projectLinkLive = document.querySelector('.project-linkLive');
+  const projectLinkSource = document.querySelector('.project-linkSource');
 
   const closeModal = () => {
     modal.classList.remove('active');
