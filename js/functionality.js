02eefa21d40a3aeb11980de2cf67f2d788bad7a0
TumiRantsane <irantsane54@gmail.com>
2022-11-25 14:56:04 +0200

    add js file

--- js/functionality.js
+++ js/functionality.js
@@ -1,32 +1,15 @@
 import { 
   menuIconMobile, menuLinks, openMenu, closeMenu 
-} from "./java.js";
+} from './java.js';
 
-import { createModal, projects } from "./popupwindow.js";
+import { createModal, projects } from './popupwindow.js';
 
-menuIconMobile.addEventListener("click", openMenu);
+menuIconMobile.addEventListener('click', openMenu);
 menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener("click", closeMenu);
+  menuLink.addEventListener('click', closeMenu);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  createModal();
 
-  const buttonsProject = document.querySelectorAll('.works__card button');
-  const closeIcon = document.querySelector('.closeModal');
-  const modal = document.querySelector('.modal');
-  const backgroundModal = document.querySelector('.overlay');
-  const projectName = document.querySelector('.project-name');
-  const projectFeature = document.querySelector('.project-feature');
-  const projectBack = document.querySelector('.project-background');
-  const projectYear = document.querySelector('.project-year');
-  const projectImage = document.querySelector('.project-image');
-  const projectDescription = document.querySelector('.project-description');
-  const projectTool1 = document.querySelector('.project-tool1');
-  const projectTool2 = document.querySelector('.project-tool2');
-  const projectTool3 = document.querySelector('.project-tool3');
-  const projectLinkLive = document.querySelector('.project-linkLive');
-  const projectLinkSource = document.querySelector('.project-linkSource');
 
   const closeModal = () => {
     modal.classList.remove('active');
@@ -35,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   buttonsProject.forEach((button) => {
     button.addEventListener('click', () => {
-      if (button.id === '0') {
+      if (button.id === "0") {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
@@ -82,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute('src', `${projects[button.id].image}`);
+        projectImage.setAttribute("src", `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
